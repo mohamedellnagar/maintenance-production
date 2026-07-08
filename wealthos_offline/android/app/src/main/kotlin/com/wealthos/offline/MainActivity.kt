072c06f45e51425fe37f8a3aab2586ef70ef423b
mohamedellnagar <116048726+mohamedellnagar@users.noreply.github.com>
2026-07-08 22:09:06 +0400

    Delete wealthos_offline directory

--- wealthos_offline/android/app/src/main/kotlin/com/wealthos/offline/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.wealthos.offline
-
-import io.flutter.embedding.android.FlutterFragmentActivity
-
-// FlutterFragmentActivity مطلوب لدعم مطالبات البصمة (local_auth) على أندرويد.
-class MainActivity : FlutterFragmentActivity()
